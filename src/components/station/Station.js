7b9ef95a3a79774c34f6d19610c91cdfb1b05c69
Asgeir Enersen <asgeir@enersen.com>
2020-06-03 19:33:56 +0200

    Revert "Initial routing test"
    This reverts commit cb5dce1cb9ec80a06fc95dfedb33d3033da6b18e.

--- src/components/station/Station.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
-function Station() {
-    const { stationId } = useParams();
-    return (<h3>Station id:{stationId}</h3>);
-}
-
-export default Station;
